123d1edc24a524660bf91da66a4bf7616a69fd75
gentedagenthe <gentedagenthe@gmail.com>
2026-09-08 21:25:31 -0400

    Delete src directory

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Questionario from './components/Questionario';
-import AdminPanel from './components/AdminPanel';
-
-export default function App() {
-  const isAdmin = window.location.pathname === '/admin';
-  return isAdmin ? <AdminPanel /> : <Questionario />;
-}
